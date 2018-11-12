d96c6041bb11bf705da9123d54a04b5f430816c4
Remco Bouckaert <higgs.ml@gmail.com>
2018-11-12 20:02:27 +1300

    fiddling with dat files

--- decisiongraphs/t.pl
+++ decisiongraphs/t.pl
@@ -1,18 +1,3 @@
-print '<svg xmlns="http://www.w3.org/2000/svg" height="340">
-<style type="text/css">
-.label {fill:#000;stroke:none;font-family:arial;font-size:10pt;font-style: normal;}
-.labeld {fill:#aaa;stroke:none;font-family:arial;font-size:10pt;font-style: normal;}
-.smalllabel {fill:#0000f0;font-size:8pt;font-family:arial;text-anchor:middle;alignment-baseline:centre;}
-.arrow {stroke:blue;stroke-width:2;fill:none;}
-</style>
-  <defs>
-    <marker id="arrow" markerWidth="10" markerHeight="10" refX="0" refY="3" orient="auto" markerUnits="strokeWidth">
-    	<g transform="matrix(0.5,0,0,0.5,0,1.5)">
-	      <path d="M0,0 L0,6 L9,3 z" fill="#00f"/>
-    	</g>
-    </marker>
-  </defs>
-';
 
 $dy = 15;
 $dx = 50;
@@ -31,13 +16,35 @@ while ($s = <>) {
 # dummy for last line condition
 $indent[$n] = -1;
 
+
+print '<svg xmlns="http://www.w3.org/2000/svg" height="'.($dy * $n + 50).'">
+<style type="text/css">
+.label {fill:#000;stroke:none;font-family:arial;font-size:10pt;font-style: normal;}
+.labeld {fill:#aaa;stroke:none;font-family:arial;font-size:10pt;font-style: normal;}
+.smalllabel {fill:#0000f0;font-size:8pt;font-family:arial;text-anchor:middle;alignment-baseline:centre;}
+.arrow {stroke:blue;stroke-width:2;fill:none;}
+</style>
+  <defs>
+    <marker id="arrow" markerWidth="10" markerHeight="10" refX="0" refY="3" orient="auto" markerUnits="strokeWidth">
+    	<g transform="matrix(0.5,0,0,0.5,0,1.5)">
+	      <path d="M0,0 L0,6 L9,3 z" fill="#00f"/>
+    	</g>
+    </marker>
+  </defs>
+';
+
+
+
+
+
 for ($i = 0; $i < $n; $i++) {
 	print "<text class=\"label\" x=\"".(40+$indent[$i]*$dx)."\" y=\"".($i*$dy+20)."\">".$s[$i]."</text>\n";
 	print "<text class=\"label\" x=\"".($i<9?"7":"0")."\" y=\"".($i*$dy+20)."\">".($i+1).".</text>\n";
 }
 
 
-for ($i = 0; $i < $n - 1; $i++) {
+for ($i = 0; $i < $n; $i++) {
+
 	if ($indent[$i] < $indent[$i + 1]) {
 		$x = 40 + $indent[$i]*$dx + 30;
 		$y = 20 + $i*$dy + 2;
@@ -62,9 +69,16 @@ for ($i = 0; $i < $n - 1; $i++) {
 			$x -= 12;
 			$y1 += 9;
 			print "<text x=\"$x\" y=\"$y1\" class=\"smalllabel\">no</text>\n";
-		}
-		
+		}		
+	} elsif ($indent[$i] == $indent[$i + 1]) {
+		$x = 40 + $indent[$i]*$dx + 30 - $dx;
+		$y = 20 + $i*$dy - 6;
+		print "<path d=\"M $x $y c 0 16,0 16, 10 16\" class=\"arrow\" marker-end=\"url(#arrow)\"/>\n";
+		$x += 12;
+		$y += 12;
+		print "<text x=\"$x\" y=\"$y\" class=\"smalllabel\">or</text>\n";
 	}
+
 }
 
 print "</svg>\n";
