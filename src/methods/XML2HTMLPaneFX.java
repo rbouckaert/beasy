c348261e593682bffa1cf5a8d7dc6020cbcd7f0c
Remco Bouckaert <higgs.ml@gmail.com>
2019-04-22 10:14:49 +1200

    add XML2HTMLPaneFX pure FX implementation

---
+++ src/methods/XML2HTMLPaneFX.java
@@ -0,0 +1,393 @@
+package methods;
+
+
+import static javafx.concurrent.Worker.State.FAILED;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import javafx.application.*;
+import javafx.beans.value.*;
+import javafx.concurrent.*;
+import javafx.concurrent.Worker.State;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.web.*;
+import methods.implementation.BEASTObjectMethodsText;
+
+import javax.swing.SwingUtilities;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.events.EventListener;
+
+import beast.app.beauti.BeautiConfig;
+import beast.app.beauti.BeautiDoc;
+import beast.app.beauti.InputFilter;
+import beast.core.BEASTInterface;
+import beast.core.MCMC;
+import beast.util.XMLParser;
+
+
+public class XML2HTMLPaneFX extends Application {
+
+	private WebEngine engine;
+	double zoom = 1.0;
+	
+	BeautiDoc beautiDoc;
+	String html;
+	List<Phrase> m;
+	XML2Text xml2textProducer;
+	
+	File tmpFile = null;
+
+	
+	public XML2HTMLPaneFX() {
+
+//		JScrollPane scroller = new JScrollPane(panel);
+//		add(scroller, BorderLayout.CENTER);
+
+
+	} // c'tor
+
+	public void processArgs(String [] args) throws Exception {		
+		beautiDoc = new BeautiDoc();
+		File file = new File(args[0]);
+		beautiDoc.setFileName(file.getAbsolutePath());
+		beautiDoc.beautiConfig = new BeautiConfig();
+		beautiDoc.beautiConfig.initAndValidate();		
+		String xml = BeautiDoc.load(file);
+		int i = xml.indexOf("beautitemplate=");
+		if (i > 0) {
+			i += 15;
+			char c = xml.charAt(i);
+			i++;
+			int start = i;
+			while (xml.charAt(i) != c) {
+				i++;
+			}
+			String template = xml.substring(start, i);
+			if (!template.endsWith("xml")) {
+				template = template + ".xml";
+			}
+			beautiDoc.loadNewTemplate(template);
+		} else {
+			beautiDoc.loadNewTemplate("Standard.xml");
+		}
+		
+		XMLParser parser = new XMLParser();
+		MCMC mcmc = (MCMC) parser.parseFile(file);
+		beautiDoc.mcmc.setValue(mcmc, beautiDoc);
+		for (BEASTInterface o : InputFilter.getDocumentObjects(beautiDoc.mcmc.get())) {
+			beautiDoc.registerPlugin(o);
+		}
+		beautiDoc.determinePartitions();
+		BEASTObjectMethodsText.setBeautiCFG(beautiDoc.beautiConfig);
+		
+		MethodsText.initNameMap();
+		initialise((MCMC) beautiDoc.mcmc.get(), true);		
+	}
+	
+	@Override
+	public void start(javafx.stage.Stage stage) throws Exception {		
+		WebView view = new WebView();
+		engine = view.getEngine();
+
+		engine.getLoadWorker().stateProperty().addListener(new ChangeListener<State>() {
+			@Override
+			public void changed(ObservableValue ov, State oldState, State newState) {
+				if (newState == Worker.State.SUCCEEDED) {
+					// note next classes are from org.w3c.dom domain
+					EventListener listener = new EventListener() {
+						@Override
+						public void handleEvent(org.w3c.dom.events.Event evt) {
+							String href = ((Element) evt.getTarget()).getAttribute("href");
+							System.out.println("link:" + href);
+							// goToLink(href);
+						}
+					};
+
+					org.w3c.dom.Document doc = engine.getDocument();
+					Element el = doc.getElementById("a");
+					NodeList lista = doc.getElementsByTagName("a");
+					for (int i = 0; i < lista.getLength(); i++) {
+						((org.w3c.dom.events.EventTarget) lista.item(i)).addEventListener("click", listener, false);
+					}
+				}
+			}
+		});
+		engine.titleProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue,
+					final String newValue) {
+				SwingUtilities.invokeLater(new Runnable() {
+					@Override
+					public void run() {
+						// System.out.println(newValue);
+					}
+				});
+			}
+		});
+
+		engine.setOnStatusChanged(new EventHandler<WebEvent<String>>() {
+			@Override
+			public void handle(final WebEvent<String> event) {
+				SwingUtilities.invokeLater(new Runnable() {
+					@Override
+					public void run() {
+						System.out.println("status changed:");
+						System.out.println(event.getData());
+//						ModelEditor me = new ModelEditor();
+//						if (me.handleCmd(event.getData(), beautiDoc, panel)) {
+//							beautiDoc.determinePartitions();
+//							beautiDoc.scrubAll(false, false);
+//							CitationPhrase.citations.clear();
+//
+//							MethodsText.clear();
+//							try {
+//								initialise((MCMC) beautiDoc.mcmc.get());
+//							} catch (Exception e) {
+//								// TODO Auto-generated catch block
+//								e.printStackTrace();
+//							}
+//						}
+					}
+				});
+			}
+		});
+
+		engine.locationProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> ov, String oldValue, final String newValue) {
+//				SwingUtilities.invokeLater(new Runnable() {
+//					@Override
+//					public void run() {
+						System.out.println("changed:");
+						System.out.println(newValue);
+						ModelEditor me = new ModelEditor(false);
+						if (me.handleCmd(newValue, beautiDoc, null)) {
+							beautiDoc.determinePartitions();
+							beautiDoc.scrubAll(false, false);
+							CitationPhrase.citations.clear();
+
+							MethodsText.clear();
+							try {
+								initialise((MCMC) beautiDoc.mcmc.get(), false);
+								load(html);
+							} catch (Exception e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					}
+//				});
+//			}
+		});
+
+		engine.getLoadWorker().exceptionProperty().addListener(new ChangeListener<Throwable>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Throwable> o, Throwable old, final Throwable value) {
+				if (engine.getLoadWorker().getState() == FAILED) {
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("Error Dialog");
+					alert.setHeaderText("Loading error...");
+					alert.setContentText((value != null) ? engine.getLocation() + "\n" + value.getMessage()
+					: engine.getLocation() + "\nUnexpected error.");
+
+					alert.showAndWait();
+				}
+			}
+		});
+
+
+//		jfxPanel.addKeyListener(new KeyListener() {
+//
+//			@Override
+//			public void keyTyped(java.awt.event.KeyEvent e) {
+//			}
+//
+//			@Override
+//			public void keyReleased(java.awt.event.KeyEvent e) {
+//				System.out.println(e);
+//				if (e.getKeyChar() == '-') {
+//					zoomOut();
+//				}
+//				if (e.getKeyChar() == '+') {
+//					zoomIn();
+//				}
+//
+//			}
+//
+//			@Override
+//			public void keyPressed(java.awt.event.KeyEvent e) {
+//			}
+//		});
+
+		Scene scene = new Scene(view, 1024, 768);
+		stage.setScene(scene);
+		stage.show();
+		
+		List<String> args = getParameters().getRaw();
+		processArgs(args.toArray(new String[]{}));
+	};
+
+
+	final static String header = "<!DOCTYPE html>\n" +
+			"<html>\n" +
+			"<style>\n" +
+			".reference {font-size:10pt;color:#aaa;}\n" + 
+			"a{color:#555;text-decoration:none;background-color:#fafafa;}\n" + 
+			".pe {color:#555;background-color:#fafafa;}\n" + 
+			".para {color:#555;background-color:#fafafa;}\n" + 
+			"select{color:#555;font-weight:normal;-webkit-appearance:none;background-color:#fafafa;border-width:5pt;}\n" + 
+			"</style>\n" +
+			"<body style='font: 12pt arial, sans-serif;'>\n";
+	
+	public void initialise(MCMC mcmc, boolean update) throws Exception {		
+		xml2textProducer = new XML2Text(beautiDoc);
+		xml2textProducer.initialise((MCMC) beautiDoc.mcmc.get());
+		m = xml2textProducer.getPhrases();
+		
+		html = header + Phrase.toHTML(beautiDoc, m) + "</body>\n</html>";
+		
+        FileWriter outfile = new FileWriter("/tmp/index.html");
+        outfile.write(html);
+        outfile.close();
+		
+        if (update) {
+        	updateState(html);
+        } else {
+        	load(html);
+        }
+	}
+
+	
+
+	
+	/**
+	 * change html text and enable/disable buttons (where appropriate) *
+	 */
+	void updateState(Object page) {
+		if (page instanceof String) {
+
+			Platform.runLater(new Runnable() {
+				@Override
+				public void run() {
+					load((String) page);
+					//engine.loadContent((String) page);
+				}
+
+			});
+			zoom(zoom);
+		} else if (page instanceof URL) {
+			Platform.runLater(new Runnable() {
+				@Override
+				public void run() {
+					String tmp = null;
+					try {
+						tmp =  new URL("" + page).toExternalForm();
+					} catch (MalformedURLException e) {
+						try {
+							tmp =  new URL("http://" + page).toExternalForm();
+						} catch (MalformedURLException e2) {
+							return;
+						}
+					}
+
+					engine.load(tmp);
+				}
+			});
+			zoom(zoom);
+		}
+	} // updateState
+
+	public void load(String page) {
+		try {
+			if (tmpFile == null) {
+				tmpFile = File.createTempFile("index", ".html");
+				System.err.println(tmpFile.getPath());
+			}
+			FileWriter outfile = new FileWriter(tmpFile);
+			outfile.write(html);
+			outfile.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// the following does not appear to be
+		// able the handle select.onchanged events
+		// but loading from file does -- not sure why
+//		engine.loadContent((String)page, "text/html");
+//		engine.setJavaScriptEnabled(true);
+		engine.load("file://" + tmpFile);
+	}
+
+	
+	private void zoomIn() {
+		zoom(zoom * 1.1);
+	}
+
+	private void zoomOut() {
+		zoom(zoom / 1.1);
+
+	}
+
+	void zoom(double _zoom) {
+		this.zoom = _zoom;
+
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				engine.executeScript("document.body.style.zoom=" + zoom);
+			}
+		});
+	}
+	
+	public String getText(CitationPhrase.mode mode) throws Exception {		
+		CitationPhrase.CitationMode = mode;
+		xml2textProducer = new XML2Text(beautiDoc);
+		xml2textProducer.initialise((MCMC) beautiDoc.mcmc.get());
+		m = xml2textProducer.getPhrases();
+		return Phrase.toString(m);
+	}
+
+	public static void main(String[] args) throws Exception {		
+		launch(args);
+		
+		
+//		
+//		JFrame frame = new JFrame();
+//		frame.setSize(700, 500);
+//		XML2HTMLPaneFX textPane = new XML2HTMLPaneFX(args);
+//		frame.add(textPane);
+////		frame.add(new XML2HTMLPane());
+//        JButton copyButton = new JButton("Copy text to clipboard");
+//        frame.add(copyButton, BorderLayout.SOUTH);
+//        copyButton.addActionListener(new ActionListener() {
+//			
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				int i = JOptionPane.showOptionDialog(frame, "Choose format", "Copy text",
+//						JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, 
+//						CitationPhrase.mode.values(), CitationPhrase.CitationMode);
+//				CitationPhrase.mode mode = CitationPhrase.mode.values()[i];
+//				StringSelection stringSelection;
+//				try {
+//					stringSelection = new StringSelection(textPane.getText(mode));
+//					Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+//					clipboard.setContents(stringSelection, null);				
+//				} catch (Exception e1) {
+//					e1.printStackTrace();
+//				}
+//			}
+//		});
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		frame.setVisible(true);
+	}
+}
