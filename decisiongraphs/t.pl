520b431e6fd4ce53a8a8cec3388d0625e9b10965
Remco Bouckaert <higgs.ml@gmail.com>
2019-11-06 08:42:48 +1300

    work a bit on decision graphs -- needs more work

--- decisiongraphs/t.pl
+++ decisiongraphs/t.pl
@@ -4,6 +4,10 @@ $dx = 50;
 
 $n = 0;
 while ($s = <>) {
+	if ($s =~ /^#(\d+):/) {
+		$id[$1] = $n + 1;
+		$s =~ s/^#(\d+)://;
+	}
 	$k = 0;
 	while (substr($s,$k,1) =~ /\s/) {
 		$k++;
@@ -38,6 +42,12 @@ print '<svg xmlns="http://www.w3.org/2000/svg" height="'.($dy * $n + 50).'">
 
 
 for ($i = 0; $i < $n; $i++) {
+	if ($s[$i] =~ /goto (\d+)/) {
+		$id = $1;
+		if ($id[$id] ne '') {
+			$s[$i] =~ s/goto \d+/goto $id[$id]/;
+		}
+	}
 	print "<text class=\"label\" x=\"".(40+$indent[$i]*$dx)."\" y=\"".($i*$dy+20)."\">".$s[$i]."</text>\n";
 	print "<text class=\"label\" x=\"".($i<9?"7":"0")."\" y=\"".($i*$dy+20)."\">".($i+1).".</text>\n";
 }
